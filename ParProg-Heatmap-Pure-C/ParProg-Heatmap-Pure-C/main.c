69cc8de8b80c5fb34be7f276263c5d2b085c2c96
Jonas <jonas@jonass-mbp.fritz.box>
2014-01-17 17:47:23 +0100

    initial xCode project

---
+++ ParProg-Heatmap-Pure-C/ParProg-Heatmap-Pure-C/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ParProg-Heatmap-Pure-C
+//
+//  Created by Jonas on 17/01/14.
+//  Copyright (c) 2014 Jonas. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
