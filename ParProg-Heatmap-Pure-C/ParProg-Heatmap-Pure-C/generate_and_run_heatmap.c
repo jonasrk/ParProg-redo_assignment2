b8abdafd3338ce2e306c5bf9fa5ff8a33887b3c9
Jonas <jonas.kemper.biz@gmail.com>
2014-03-27 15:03:06 +0100

    default bracket concept

--- ParProg-Heatmap-Pure-C/ParProg-Heatmap-Pure-C/generate_and_run_heatmap.c
+++ ParProg-Heatmap-Pure-C/ParProg-Heatmap-Pure-C/generate_and_run_heatmap.c
@@ -7,9 +7,7 @@ void* run_heatmap_tile(void* args){
 	        for (int col = -1; col < 2; col++){
 	            if (x+col >= 0 && x+col < this_args->width && this_args->y+row >= 0 && this_args->y+row < this_args->height){ //checks if cell is inside heatmap
 	                sum += this_args->last_round[(x+col)*this_args->height+this_args->y+row];}}}
-	    this_args->heatmap[(x*this_args->height)+this_args->y] = sum/9.0;}
-	
-		}
+	        this_args->heatmap[(x*this_args->height)+this_args->y] = sum/9.0;}}
 	pthread_exit(NULL);}
 
 
@@ -26,37 +24,22 @@ double* generate_and_run_heatmap(int width, int height, int rounds, int number_o
     for (int round = 0; round < rounds; round++) {
         heatmap = malloc(sizeof(double)*width*height);
         for (int y = 0; y < height; y++) {
-			
 			pthread_t thread_ids[nprocs_max];
 			struct heatmap_tile_args these_args[nprocs_max];
-			
 			for (int thread = 0; thread < nprocs_max; thread++){
-				
 				these_args[thread].width = width;
 				these_args[thread].height = height;
 				these_args[thread].y = y;
 				these_args[thread].thread = thread;
-				
 				these_args[thread].heatmap = heatmap;
 				these_args[thread].last_round = last_round;
 				these_args[thread].nprocs_max = nprocs_max;
+				pthread_create(&thread_ids[thread], NULL, run_heatmap_tile, &these_args[thread]);}
 				
-				pthread_create(&thread_ids[thread], 
-					                    NULL,
-					                    run_heatmap_tile,
-					                    &these_args[thread]);
+			for (int thread = 0; thread < nprocs_max; thread++){
+				pthread_join(thread_ids[thread], NULL);}}
 				
-				}
-				
-				for (int thread = 0; thread < nprocs_max; thread++){
-					pthread_join(thread_ids[thread], NULL);}
-			
-			}
-				
-				
-				
-				
-        for (int hotspot = 0; hotspot < number_of_hotspots; hotspot++){
+	 for (int hotspot = 0; hotspot < number_of_hotspots; hotspot++){
             if (round >= hotspots[hotspot*4+2] && round < hotspots[hotspot*4+3]){
                 heatmap[(hotspots[hotspot*4]*height)+hotspots[hotspot*4+1]] = 1;}}
         last_round = heatmap;}
