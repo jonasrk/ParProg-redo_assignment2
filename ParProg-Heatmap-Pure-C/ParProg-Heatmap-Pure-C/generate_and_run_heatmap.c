d7f0f1cea0707e1d6087662d2d261ba11f2fc416
Jonas <jonas.kemper.biz@gmail.com>
2014-03-27 14:50:55 +0100

    validation works again. now correctly threaded??

--- ParProg-Heatmap-Pure-C/ParProg-Heatmap-Pure-C/generate_and_run_heatmap.c
+++ ParProg-Heatmap-Pure-C/ParProg-Heatmap-Pure-C/generate_and_run_heatmap.c
@@ -1,12 +1,14 @@
 void* run_heatmap_tile(void* args){
 	
 	
-		
+	printf("Thread here. *args is %i\n", args);
 	
-	    struct heatmap_tile_args* this_args;
-		this_args = args;
+	    struct heatmap_tile_args* this_args = args;
+		
+		//printf("Thread %i here. this_args is %i\n", this_args);
 
 		int width = this_args->width;
+		//printf("\nThread here. this_args.width is %i.\n", this_args.width);
 		int height = this_args->height;
 		int y = this_args->y;
 		int thread = this_args->thread;
@@ -14,8 +16,13 @@ void* run_heatmap_tile(void* args){
 		double* last_round = this_args->last_round;
 		long nprocs_max = this_args->nprocs_max;
 		
+		//printf("*** Thread %i here. My this_args_pointer is %i\n", thread, this_args_pointer);
+		printf("*** Thread %i here. My this_args lies at %i\n", thread, &this_args);
+		
 	    for (int x = 0; x < width; x++){
+			printf("x is %i, width is %i.\n");
 			if (x % nprocs_max == thread){
+				printf("Thread %i computing %i.\n", thread, x);
 	        double sum = 0;
 	        for (int row = -1; row < 2; row++){
 	            for (int col = -1; col < 2; col++){
@@ -46,11 +53,17 @@ double* generate_and_run_heatmap(int width, int height, int rounds, int number_o
 			
 			for (int thread = 0; thread < nprocs_max; thread++){
 				
-				struct heatmap_tile_args these_args[thread];
+				printf("Hallo Sarah! Ich erstelle nun Thread %i\n", thread);
+				
+				
+				printf("\n*** Host here. these_args for Thread %i lies at %i\n", thread, &these_args[thread]);
+				
 				these_args[thread].width = width;
+				printf("(*(&these_args[thread])).width is %i\n", (*(&these_args[thread])).width);
 				these_args[thread].height = height;
 				these_args[thread].y = y;
 				these_args[thread].thread = thread;
+				
 				these_args[thread].heatmap = heatmap;
 				these_args[thread].last_round = last_round;
 				these_args[thread].nprocs_max = nprocs_max;
@@ -59,8 +72,12 @@ double* generate_and_run_heatmap(int width, int height, int rounds, int number_o
 					                    NULL,
 					                    run_heatmap_tile,
 					                    &these_args[thread]);
-				pthread_join(thread_ids[0], NULL);
+				
 				}
+				
+				for (int thread = 0; thread < nprocs_max; thread++){
+					printf("Waiting for thread %i to join.\n", thread);
+					pthread_join(thread_ids[thread], NULL);}
 			
 			}
 				
