968b966f530ae51de52095f7e8b6c0a856cb8275
Jonas <jonas.kemper.biz@gmail.com>
2014-03-29 12:34:03 +0100

    own file for run tile

--- ParProg-Heatmap-Pure-C/ParProg-Heatmap-Pure-C/generate_and_run_heatmap.c
+++ ParProg-Heatmap-Pure-C/ParProg-Heatmap-Pure-C/generate_and_run_heatmap.c
@@ -1,16 +1,4 @@
-void* run_heatmap_tile(void* args){
-	struct heatmap_tile_args* this_args = args;
-	for (int y = 0; y < this_args->height; y++) {
-		if (y % this_args->nprocs_max == this_args->thread){
-			for (int x = 0; x < this_args->width; x++){
-	    		double sum = 0;
-	    	    for (int row = -1; row < 2; row++){
-	    	        for (int col = -1; col < 2; col++){
-	    	            if (x+col >= 0 && x+col < this_args->width && y+row >= 0 && y+row < this_args->height){ //checks if cell is inside heatmap
-	    	                sum += this_args->last_round[(x+col)*this_args->height+y+row];}}}
-	    	        this_args->heatmap[(x*this_args->height)+y] = sum/9.0;}}}
-	pthread_exit(NULL);}
-
+#include "run_heatmap_tile.c"
 
 double* generate_and_run_heatmap(int width, int height, int rounds, int number_of_hotspots, int* hotspots, long nprocs_max){
     // generate heatmap
---
+++ ParProg-Heatmap-Pure-C/ParProg-Heatmap-Pure-C/run_heatmap_tile.c
@@ -0,0 +1,14 @@
+void* run_heatmap_tile(void* args){
+	struct heatmap_tile_args* this_args = args;
+	for (int y = 0; y < this_args->height; y++) {
+		if (y % this_args->nprocs_max == this_args->thread){
+			for (int x = 0; x < this_args->width; x++){
+	    		double sum = 0;
+	    	    for (int row = -1; row < 2; row++){
+	    	        for (int col = -1; col < 2; col++){
+	    	            if (x+col >= 0 && x+col < this_args->width && y+row >= 0 && y+row < this_args->height){ //checks if cell is inside heatmap
+	    	                sum += this_args->last_round[(x+col)*this_args->height+y+row];}}}
+	    	        this_args->heatmap[(x*this_args->height)+y] = sum/9.0;}}}
+	pthread_exit(NULL);}
+
+
